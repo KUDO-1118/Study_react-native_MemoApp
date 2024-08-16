7ef8891cc55d28b52c911ca54fb358e092bcf7db
{KUDO} <au4saf010@gmail.com>
2024-08-16 16:31:01 +0900

    プロパティを受け渡す(TextStyle)

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,16 +1,18 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, type TextStyle } from 'react-native'
 
 interface Props {
   children: string
   bang?: boolean
-  // bang?はオプションとして扱うので使っても使わなくても良い
+  // bang?の?はオプションとして扱うので使っても使わなくても良い
+  style?: TextStyle
 }
 
 const Hello = (props: Props): JSX.Element => {
-  const { children, bang } = props
+  const { children, bang, style } = props
   return (
     <View>
-      <Text style={styles.text}>
+      <Text style={[styles.text, style]}>
+        {/* styleの上書きの優先順位は右から */}
         Hello {children}{bang === true ? '!' : ''}
         {/* ===は等しいという意味 */}
       </Text>
