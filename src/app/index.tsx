734290eafe06186375f7e7b2c6c4838d7c6b76f9
{KUDO} <au4saf010@gmail.com>
2024-08-17 18:11:47 +0900

    Header Styling

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,13 +2,15 @@ import { View, Text, StyleSheet } from 'react-native'
 
 const Index = (): JSX.Element => {
   return (
-    <View style = {styles.container}>
-      <View>
-        <View>
-          <Text>Memo App</Text>
-          <Text>ログアウト</Text>
+    <View style={styles.container}>
+      
+      <View style={styles.header}>
+        <View style={styles.Inner}>
+          <Text style={styles.headerTitle}>Memo App</Text>
+          <Text style={styles.headerRight}>ログアウト</Text>
         </View>
       </View>
+      
       <View>
 
         <View>
@@ -53,10 +55,34 @@ const Index = (): JSX.Element => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    // justifyContentは上下
+    backgroundColor: '#ffffff'
+    // justifyContent: 'center',
+    // // justifyContentは上下
+    // alignItems: 'center'
+    // // alignItemsは左右
+  },
+  header: {
+    backgroundColor: '#467FD3',
+    height: 104,
+    justifyContent: 'flex-end',
+  },
+  headerInner: {
     alignItems: 'center'
-    // alignItemsは左右
+  },
+  headerRight: {
+    position: 'absolute',
+    right: 16,
+    //右方向
+    bottom: 16,
+    //下方向
+    color: 'rgba(255, 255, 255, 0.7)'
+  },
+  headerTitle: {
+    marginBottom: 8,
+    fontSize: 22,
+    lineHeight: 32,
+    fontWeight: 'bold',
+    color: '#ffffff'
   }
 })
 
