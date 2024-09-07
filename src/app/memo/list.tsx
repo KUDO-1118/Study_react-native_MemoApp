4d9ca180c6f69f55f0d1bf4a7c3561549dea7385
{KUDO} <au4saf010@gmail.com>
2024-09-07 12:58:36 +0900

    データの方を定義する

--- src/app/memo/list.tsx
+++ src/app/memo/list.tsx
@@ -1,6 +1,6 @@
 import { View, StyleSheet } from 'react-native'
 import { router, useNavigation } from 'expo-router'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { collection, onSnapshot, query, orderBy } from 'firebase/firestore'
 
 import MemoListItem from '../../components/MemoListitem'
@@ -8,12 +8,14 @@ import CircleButton from '../../components/CircleButton'
 import Icon from '../../components/icon'
 import LogOutButton from '../../components/LogOutButton'
 import { db, auth } from '../../config'
+import { type Memo } from '../../../types/memo'
 
 const handlePress = (): void => {
   router.push('/memo/create')
 }
 
 const List = (): JSX.Element => {
+  const [memos, setMemos] = useState<Memo[]>([])//データを保持する
   const navigation = useNavigation()
   useEffect(() => {
     navigation.setOptions({
@@ -27,9 +29,17 @@ const List = (): JSX.Element => {
     const ref = collection(db, `users/${auth.currentUser.uid}/memos`)
     const q = query(ref, orderBy('updatedAt', 'desc'))
     const unsubscribe = onSnapshot(q, (snapshot) => {
+      const remoteMemos: Memo[] = [] //一時保存
       snapshot.forEach((doc) => {
         console.log('memo', doc.data())
+        const { bodyText, updatedAt } = doc.data()
+        remoteMemos.push({//pushメソッドは配列にデータを１つ入れる
+          id: doc.id,
+          bodyText,
+          updatedAt
+        })
       })
+      setMemos(remoteMemos)
     })
     return unsubscribe
   }, [])
