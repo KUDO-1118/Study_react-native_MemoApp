9428a608212b6ebf2f84b1ae7412b867f7d17d8b
{KUDO} <au4saf010@gmail.com>
2024-09-08 14:27:56 +0900

    画面遷移時にパラメータを渡す

--- src/app/memo/detail.tsx
+++ src/app/memo/detail.tsx
@@ -1,5 +1,5 @@
 import { View, Text, ScrollView, StyleSheet } from 'react-native'
-import { router } from 'expo-router'
+import { router, useLocalSearchParams } from 'expo-router'
 
 import CircleButton from '../../components/CircleButton'
 import Icon from '../../components/icon'
@@ -9,6 +9,8 @@ const handlePress = (): void => {
 }
 
 const Detail = (): JSX.Element => {
+  const { id } = useLocalSearchParams() //idを取得
+  console.log(id)
   return (
     <View>
       <View style={styles.memoMeader}>
