90f00c8fcde286040f78d7574aff7843471ed31a
{KUDO} <au4saf010@gmail.com>
2024-09-08 17:22:35 +0900

    メモの編集画面の実装

--- src/app/memo/detail.tsx
+++ src/app/memo/detail.tsx
@@ -8,17 +8,17 @@ import Icon from '../../components/icon'
 import { auth, db } from '../../config'
 import { type Memo } from '../../../types/memo'
 
-const handlePress = (): void => {
-  router.push('/memo/edit')
+const handlePress = (id: string): void => {
+  router.push({ pathname: '/memo/edit', params: { id } })
 }
 
 const Detail = (): JSX.Element => {
-  const { id } = useLocalSearchParams() //idを取得
+  const  id = String(useLocalSearchParams().id) //idを取得
   console.log(id)
   const [memo, setMemo] = useState<Memo | null>(null)
   useEffect(() => {
     if (auth.currentUser === null) { return }
-    const ref = doc(db, `users/${auth.currentUser.uid}/memos`, String(id))//String(id)はdocのid
+    const ref = doc(db, `users/${auth.currentUser.uid}/memos`, id)//String(id)はdocのid
     const unsubscribe = onSnapshot(ref, (memoDoc) => {
       // console.log(memoDoc.data())
       const { bodyText, updateAt } = memoDoc.data() as Memo
@@ -41,7 +41,7 @@ const Detail = (): JSX.Element => {
           { memo?.bodyText }
         </Text>
       </ScrollView>
-      <CircleButton onPress={handlePress} style={{ top: 60, bottom: 'auto' }}>
+      <CircleButton onPress={() => { handlePress(id) }} style={{ top: 60, bottom: 'auto' }}>
         <Icon name='pencil' size={40} color='#ffffff' />
       </CircleButton>
     </View>
